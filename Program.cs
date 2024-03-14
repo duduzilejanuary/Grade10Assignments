4ebae7ca61defa88f22cead8250c43a11979010d
Duduzile January <duduzile.jan@gmail.com>
2024-03-14 23:29:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+//Duduzile January's Take on Microsoft Guided Project 1
+using Grade10Assignments;
+
+Console.Title = "Grade 10 Student Assignments";
+Console.WriteLine("Grade 10 Mathematics Assignments\n");
+Console.WriteLine("Student Name\t\t\t\tAssignment Topic\t\t\tGrade\n=======================================================================================");
+
+StudentAssignment[] studentAssignments = new StudentAssignment[]
+{
+    new StudentAssignment("Amahle Heleni", "Algebra",90),
+    new StudentAssignment("Ndalo January", "Algebra",96),
+    new StudentAssignment("Sino Langa", "Algebra",98),
+    new StudentAssignment("Mbali Mnguni", "Algebra",90),
+};
+for (int i = 0; i < studentAssignments.Length; i++)
+{
+    var assignment = studentAssignments[i];
+    Console.WriteLine($"{assignment.StudentName}\t\t\t\t{assignment.AssignmentTopic}\t\t\t\t\t{assignment.Grade}");
+}
+Console.WriteLine("\n\nTotal Number of Students: {0}", studentAssignments.Length);
+
+
+Console.ReadLine();
---
+++ StudentAssignment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grade10Assignments
+{
+    public class StudentAssignment
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string AssignmentTopic {  get; set; }
+        public int Grade { get; set; }
+
+        public StudentAssignment(string studentName, string assignmentTopic, int grade)
+        {
+            StudentName = studentName;
+            AssignmentTopic = assignmentTopic;
+            Grade = grade;
+        }
+    }
+}
